cea886d24707faa9e2e8cff259b576754209f6ac
Laurent <lezariant@gmail.com>
2023-02-01 19:03:32 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -69,12 +69,16 @@ using namespace sensesp;
 // Initiate I2C : used for display and BMP280
 TwoWire* i2c;
 Adafruit_SSD1306* display;
+Adafruit_BMP280 bmp280;
 const float Vin = 3.3;
 
 
-Adafruit_BMP280 bmp280;
-float read_temp_callback() { return (bmp280.readTemperature() + 273.15);}
-float read_pressure_callback() { return (bmp280.readPressure());}
+float read_temp_callback() {
+    return (bmp280.readTemperature() + 273.15);
+}
+float read_pressure_callback() {
+    return (bmp280.readPressure());
+}
 
 /// Clear a text row on an Adafruit graphics display
 void ClearRow(int row) { display->fillRect(0, 8 * row, SCREEN_WIDTH, 8, 0); }
@@ -84,6 +88,7 @@ float KelvinToCelsius(float temp) { return temp - 273.15; }
 void displayData(int row, String title, float value) {
   ClearRow(row);
   display->setCursor(0, 8 * row);
+  
   display->printf("%s: %.1f", title.c_str(), TEMP_DISPLAY_FUNC(value));
   display->display();
 }
@@ -95,28 +100,27 @@ class TankCapacityInterpreter : public CurveInterpolator {
       : CurveInterpolator(NULL, config_path) {
     // Populate a lookup table to translate measure into a %
     clear_samples();
-    /*
-    add_sample(CurveInterpolator::Sample(9.6, 1));
-    add_sample(CurveInterpolator::Sample(11.5, 0.75));
-    add_sample(CurveInterpolator::Sample(14.1, 0.50));
-    add_sample(CurveInterpolator::Sample(18.2, 0.25));
-    add_sample(CurveInterpolator::Sample(1.5, 0.0));
-    */
-   add_sample(CurveInterpolator::Sample(1.50,0.0));
-   add_sample(CurveInterpolator::Sample(20.35,0.1));
-   add_sample(CurveInterpolator::Sample(39.20,0.2));
-   add_sample(CurveInterpolator::Sample(58.05,0.3));
-   add_sample(CurveInterpolator::Sample(76.90,0.4));
-   add_sample(CurveInterpolator::Sample(95.75,0.5));
-   add_sample(CurveInterpolator::Sample(114.60,0.6));
-   add_sample(CurveInterpolator::Sample(133.45,0.7));
-   add_sample(CurveInterpolator::Sample(152.30,0.8));
-   add_sample(CurveInterpolator::Sample(171.15,0.9));
-   add_sample(CurveInterpolator::Sample(190.00,1.0));
+    add_sample(CurveInterpolator::Sample(0, 0.0));
+    add_sample(CurveInterpolator::Sample(36, 0.0));
+    add_sample(CurveInterpolator::Sample(40, 0.0));
+    add_sample(CurveInterpolator::Sample(44, 0.0));
+    add_sample(CurveInterpolator::Sample(200, 0.3448));
+    add_sample(CurveInterpolator::Sample(355, 0.125));
+    add_sample(CurveInterpolator::Sample(433, 0.25));
+    add_sample(CurveInterpolator::Sample(466, 0.375));
+    add_sample(CurveInterpolator::Sample(522, 0.5));
+    add_sample(CurveInterpolator::Sample(568, 0.625));
+    add_sample(CurveInterpolator::Sample(588, 0.75));
+    add_sample(CurveInterpolator::Sample(637.0, 0.875));
+    add_sample(CurveInterpolator::Sample(675.0, 1));
+    add_sample(CurveInterpolator::Sample(680.0, 1));
+    add_sample(CurveInterpolator::Sample(685.0, 1));
+    add_sample(CurveInterpolator::Sample(1000.0, 1));
   }
 };
 
 // **NOT IMPLEMENTED YET ** Fuel Consomption interpreter based on engine RPM
+// Provide fuel consumption related to RPM
 class FuelInterpreter : public CurveInterpolator {
  public:
   FuelInterpreter(String config_path = "")
@@ -182,15 +186,9 @@ SensESPAppBuilder builder;
                     // settings. This is normally not needed.
 //                    ->set_wifi("Stelamaya", "@Helios01!")
                     ->set_wifi("Livebox-Bignon", "0123456789ABCDEF9876543210")
-//                    ->set_wifi("Nova_Wifi", "@Helios01!")
 //                    ->set_sk_server("stelamayarpi4.local", 3000)
 //                    ->set_sk_server("themis-m93p.local", 3000)
-                    //->set_wifi("Livebox-Bignon", "0123456789ABCDEF9876543210")
-                    ->set_sk_server("stelamayarpi4.local", 3000)
-                    // Client ID for Themis (home) : f41e7137-074a-bea3-bb6d-0fea8c75ce18
-
-                    //->set_sk_server("stelamayarpi4.local", 3000)
-                    // Client ID for stelamayarpi4 : 9ce8187e-191c-21bb-faf1-40f884d25cb5
+                    ->set_sk_server("192.168.1.59", 3000)
 
                     ->enable_uptime_sensor()
                     ->get_app();
@@ -201,23 +199,31 @@ SensESPAppBuilder builder;
 ***************************************************************************/
   i2c = new TwoWire(0);
   i2c->begin(SDA_PIN, SCL_PIN);
+
   display = new Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, i2c, -1);
   if (!display->begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
+    Serial.println(F("SSD1306 not found !"));
+  } else {
+    delay(100);
+    display->clearDisplay();
+    display->setTextSize(1);
+    display->setTextColor(SSD1306_WHITE);
+    display->setCursor(0, 0);
+    display->printf("Host: %s", sensesp_app->get_hostname().c_str());
+//    display->printf("IP: %s",WiFi.localIP().toString().c_str());
+    display->display();
+    delay(1000);
   }
-  delay(100);
-  //display->setRotation(2);
-  display->clearDisplay();
-  display->setTextSize(1);
-  display->setTextColor(SSD1306_WHITE);
-  display->setCursor(0, 0);
-  display->printf("Host: %s", sensesp_app->get_hostname().c_str());
-  display->display();
-  
 /***************************************************************************
 * BMP280 Sensor : to get environment : temperature, pressure
 ***************************************************************************/
-bmp280.begin(0x77);
+/*
+//if (!bmp280.begin(0x77)){
+if (!bmp280.begin()){
+    display->setCursor(0, 16);
+    display->printf("%s", "BMP280 not found !");
+    display->display();
+} else {
 auto* main_engine_bay_temperature = new RepeatSensor<float>(5000, read_temp_callback);
 auto* main_engine_bay_pressure = new RepeatSensor<float>(60000, read_pressure_callback);
 
@@ -230,7 +236,7 @@ auto main_engine_bay_temperature_metadata =
     );
 main_engine_bay_temperature
     ->connect_to(new SKOutputFloat("propulsion.engineBay.temperature", "/mainEngineBayTemperature/skPath", main_engine_bay_temperature_metadata))
-    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(2, "Bay T: ", temperature); }));
+    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(5, "Bay T: ", temperature); }));
 
 auto main_engine_bay_pressure_metadata =
     new SKMetadata("Pa",            // units
@@ -242,16 +248,8 @@ auto main_engine_bay_pressure_metadata =
 
 main_engine_bay_pressure
     ->connect_to(new SKOutputFloat("propulsion.engineBay.pressure", "/mainEngineBayPressure/skPath", main_engine_bay_pressure_metadata))
-    ->connect_to(new LambdaConsumer<float>([](float pressure) { displayData(3, "Bay P: ", pressure); }));
-
-
-/*
-    // 0x77 is the default address. Some chips use 0x76, which is shown here.
-    // If you need to use the TwoWire library instead of the Wire library, there
-    // is a different constructor: see bmp280.h
-    
-
-
+    ->connect_to(new LambdaConsumer<float>([](float pressure) { displayData(7, "Bay P: ", pressure); }));
+}
 */
 
 /***************************************************************************
@@ -283,8 +281,8 @@ main_engine_coolant_temperature
     //->connect_to(new AnalogVoltage()) // Never succeed in getting valuable values using these transformations
     //->connect_to(new VoltageDividerR2(Rt1, Vin, "/EngineTemperature/sender"))
     ->connect_to(getTemperatureFromResistance_transform)
-    ->connect_to(new SKOutputFloat("propulsion.main.coolantTemperature", "/mainEngineCoolantTemperature/sk_path", main_engine_coolant_temperature_metadata)) // send to SignalK
-    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(2, "Coolant", temperature); })); // send to display
+    ->connect_to(new SKOutputFloat("propulsion.engine.coolantTemperature", "/mainEngineCoolantTemperature/sk_path", main_engine_coolant_temperature_metadata)) // send to SignalK
+    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(6, "Coolant", temperature); })); // send to display
 */
 
 
@@ -309,9 +307,11 @@ auto main_engine_exhaust_temperature_metadata =
     );
 // propulsion.*.wetExhaustTemperature is a non-standard path
 main_engine_exhaust_temperature
-    ->connect_to(new SKOutputFloat("propulsion.main.exhaustTemperature", "/mainEngineExhaustTemperature/skPath", main_engine_exhaust_temperature_metadata))
-    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(5, "Exhaust", temperature); }));
+    ->connect_to(new SKOutputFloat("propulsion.engine.exhaustTemperature", "/mainEngineExhaustTemperature/skPath", main_engine_exhaust_temperature_metadata))
+    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(4, "Exhaust", temperature); }));
 
+
+/*=============================================================*/
 /* Alternative to BMP280 sensor using DS18B20
 auto main_engine_bay_temperature =
       new OneWireTemperature(dts, 5000, "/mainEngineBayTemp/oneWire");
@@ -323,10 +323,11 @@ auto main_engine_bay_temperature_metadata =
                 10.                         // timeout, in seconds
     );
 main_engine_bay_temperature
-    ->connect_to(new SKOutputFloat("propulsion.main.temperature", "/mainEngineBayTemperature/skPath", main_engine_bay_temperature_metadata))
-    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(3, "Engine Bay", temperature); }));
+    ->connect_to(new SKOutputFloat("propulsion.engineBay.temperature", "/mainEngineBayTemperature/skPath", main_engine_bay_temperature_metadata))
+    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(5, "Engine Bay", temperature); }));
 */
 
+/*=============================================================*/
 /* Alternative to FAE31020 sensor using DS18B20
 auto main_engine_coolant_temperature =
       new OneWireTemperature(dts, 5000, "/mainCoolantTemp/oneWire");
@@ -338,8 +339,8 @@ auto main_engine_coolant_temperature_metadata =
                 10.                             // timeout, in seconds
     );
 main_engine_coolant_temperature
-    ->connect_to(new SKOutputFloat("propulsion.main.coolantTemperature", "/mainEngineCoolantTemperature/skPath", main_engine_coolant_temperature_metadata))
-    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(5, "Coolant", temperature); }));
+    ->connect_to(new SKOutputFloat("propulsion.engine.coolantTemperature", "/mainEngineCoolantTemperature/skPath", main_engine_coolant_temperature_metadata))
+    ->connect_to(new LambdaConsumer<float>([](float temperature) { displayData(6, "Coolant", temperature); }));
 */
 
 
@@ -351,6 +352,37 @@ auto* main_engine_tank_currentVolume = new AnalogInput(FUEL_GAUGE_PIN, 5000);
 const float Rc1 = 100;
 
 const float CAPACITY = 90; // Capacity in 1000xLiters
+auto* Capacity=new Configurable("Diesel Tank Capacity", "Capacity of Diesel tank in Liters");
+
+auto main_engine_tank_level_metadata =
+    new SKMetadata("%",                   // units
+                "Diesel Tank remaining Level",  // display name
+                "Diesel Tank remaining Level",  // description
+                "Tank Level",         // short name
+                10.                    // timeout, in seconds
+    );
+
+
+main_engine_tank_currentVolume
+    ->connect_to(new TankCapacityInterpreter("/FuelTank/capacity/curve"))
+//    ->connect_to(new Linear(90.0,0))
+    ->connect_to(new SKOutputFloat("self.tanks.fuel.main.currentVolume", "/mainEngineTankCapacity/skPath", main_engine_tank_level_metadata));
+//    ->connect_to(new LambdaConsumer<float>([](float capacity) { displayData(2, "Tank Capacity", capacity+273.15); })); // add manually 273.15 because it is not a temperature.
+
+//FIN TEST avec exemple du net
+
+
+/*
+main_engine_tank_currentVolume
+    ->connect_to(new AnalogVoltage())
+    ->connect_to(new VoltageDividerR2(Rc1, Vin, "/FuelTank/capacity/sender"))
+    //->connect_to(new TankCapacityInterpreter("/FuelTank/capacity/curve"))
+    //->connect_to(new Linear(CAPACITY, 0.0, "/FuelTank/capacity/calibrate"))
+    ->connect_to(new SKOutputFloat("self.tanks.fuel.main.currentVolume", "/mainEngineTankCapacity/skPath", main_engine_tank_level_metadata))
+    ->connect_to(new LambdaConsumer<int>([](int capacity) { displayData(2, "Tank Capacity", capacity+273.15); })); // add manually 273.15 because it is not a temperature.
+*/
+
+
 
 /* A retravailler
 auto main_engine_tank_capacity_metadata =
@@ -366,21 +398,9 @@ main_engine_tank_capacity
     ->connect_to(new LambdaConsumer<int>([](int capacity));
 */
 
-auto main_engine_tank_level_metadata =
-    new SKMetadata("L",                   // units
-                "Diesel Tank remaining Level",  // display name
-                "Diesel Tank remaining Level",  // description
-                "Tank Level",         // short name
-                10.                    // timeout, in seconds
-    );
 
-main_engine_tank_currentVolume
-    ->connect_to(new AnalogVoltage())
-    ->connect_to(new VoltageDividerR2(Rc1, Vin, "/FuelTank/capacity/sender"))
-    ->connect_to(new TankCapacityInterpreter("/FuelTank/capacity/curve"))
-    ->connect_to(new Linear(CAPACITY, 0.0, "/FuelTank/capacity/calibrate"))
-    ->connect_to(new SKOutputInt("self.tanks.fuel.main.currentVolume", "/mainEngineTankCapacity/skPath", main_engine_tank_level_metadata))
-    ->connect_to(new LambdaConsumer<int>([](int capacity) { displayData(6, "Tank Capacity", capacity+273.15); })); // add manually 273.15 because it is not a temperature.
+
+
 
 /***************************************************************************
 * Engine RPM : get value from alternator rotation speed
@@ -410,7 +430,7 @@ auto main_engine_consumption_metadata =
 main_engine_rpm
     ->connect_to(new Frequency(multiplier, config_path_calibrate))  
     // connect the output of sensor to the input of Frequency()
-    ->connect_to(new SKOutputFloat("propulsion.main.revolutions", "/mainEngineRPM/skPath", main_engine_rpm_metadata)
+    ->connect_to(new SKOutputFloat("propulsion.engine.revolutions", "/mainEngineRPM/skPath", main_engine_rpm_metadata)
     );  
     
 // Calculate fuel consumption
@@ -418,8 +438,8 @@ main_engine_rpm
     ->connect_to(new Frequency(6))
     // times by 6 to go from Hz to RPM
     ->connect_to(new FuelInterpreter("/Engine Fuel/curve"))
-    ->connect_to(new SKOutputFloat("propulsion.engine.fuelconsumption", "/mainEngineConsumption/sk_path", main_engine_consumption_metadata))
-    ->connect_to(new LambdaConsumer<int>([](int speed) { displayData(7, "Engine RPM", speed+273.15); })); // add manually 273.15 because it is not a temperature.
+    ->connect_to(new SKOutputFloat("propulsion.engine.fuel.rate", "/mainEngineConsumption/sk_path", main_engine_consumption_metadata))
+    ->connect_to(new LambdaConsumer<int>([](int speed) { displayData(3, "Engine RPM", speed+273.15); })); // add manually 273.15 because it is not a temperature.
 
 
 
